5638ee12e8714f764c84adb1ca140d8580ad549d
sudha-guragol <110382111+sudha-guragol@users.noreply.github.com>
2022-08-31 15:56:33 +0530

    Controller based Exceptional Handling

---
+++ Controller-Based-Exception-Handling/src/main/java/com/example/demo/ControllerBasedExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerBasedExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerBasedExceptionHandlingApplication.class, args);
+	}
+
+}
---
+++ Controller-Based-Exception-Handling/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+	
+	
+	@RequestMapping("/welcome")
+	public String welcome(Model model)
+	{
+		model.addAttribute("Msg", "welcome to Ashok IT");
+		String name=null;
+		name.length();
+		return "welcome";
+	}
+
+	
+	//handling controller level exception
+	@ExceptionHandler(value=NullPointerException.class)
+	public String handleNullPointerException(Model model)
+	{
+		model.addAttribute("errMsg", "some problem occured,please try after sometime!!!!");
+		return "error";
+	}
+}
